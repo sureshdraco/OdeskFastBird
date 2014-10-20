87cbd82c0773b311442b0ae1ee617ac15c35ad0a
suresh <suresh.kumar@rebtel.com>
2014-10-20 09:35:19 +0200

    Revert "fixed receiving gcm msgs"
    This reverts commit 2f8cc0e1f38b7346efc9d72cb2eee30b37c0df15.

--- FastBird/app/src/main/java/appinventor/ai_sameh/FastBird/receiver/GcmBroadcastReceiver.java
+++ FastBird/app/src/main/java/appinventor/ai_sameh/FastBird/receiver/GcmBroadcastReceiver.java
@@ -5,17 +5,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
-import android.util.Log;
 
 import appinventor.ai_sameh.FastBird.service.GcmIntentService;
 
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
-    private static final String TAG = GcmBroadcastReceiver.class.getSimpleName();
-
-    @Override
+    @Override 
     public void onReceive(Context context, Intent intent) {
-       Log.d(TAG, "received");
-        // Explicitly specify that GcmIntentService will handle the intent.
+        // Explicitly specify that GcmIntentService will handle the intent. 
         ComponentName comp = new ComponentName(context.getPackageName(),
                 GcmIntentService.class.getName());
         // Start the service, keeping the device awake while it is launching. 
