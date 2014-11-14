909c58a38cf4333e7eddff4281cce37b5aa26ad9
suresh <suresh.kumar@rebtel.com>
2014-11-14 15:32:05 +0100

    more tasks.

--- FastBird/app/src/main/java/appinventor/ai_sameh/FastBird/adapter/CashDetailListArrayAdapter.java
+++ FastBird/app/src/main/java/appinventor/ai_sameh/FastBird/adapter/CashDetailListArrayAdapter.java
@@ -22,51 +22,55 @@ import appinventor.ai_sameh.FastBird.api.model.Order;
 import appinventor.ai_sameh.FastBird.view.CommentActivity;
 
 public class CashDetailListArrayAdapter extends ArrayAdapter<MoneyDetail> {
-    private List<MoneyDetail> moneyDetails = new ArrayList<MoneyDetail>();
-    private Context context;
+	private List<MoneyDetail> moneyDetails = new ArrayList<MoneyDetail>();
+	private Context context;
 
-    static class OrderViewHolder {
-        TextView fbdNumber, amount;
-    }
+	static class OrderViewHolder {
+		TextView fbdNumber, amount, collectionAmount, serviceFee;
+	}
 
-    public CashDetailListArrayAdapter(Context context, int textViewResourceId) {
-        super(context, textViewResourceId);
-        this.context = context;
-    }
+	public CashDetailListArrayAdapter(Context context, int textViewResourceId) {
+		super(context, textViewResourceId);
+		this.context = context;
+	}
 
-    @Override
-    public void add(MoneyDetail object) {
-        moneyDetails.add(object);
-        super.add(object);
-    }
+	@Override
+	public void add(MoneyDetail object) {
+		moneyDetails.add(object);
+		super.add(object);
+	}
 
-    @Override
-    public int getCount() {
-        return this.moneyDetails.size();
-    }
+	@Override
+	public int getCount() {
+		return this.moneyDetails.size();
+	}
 
-    @Override
-    public MoneyDetail getItem(int index) {
-        return this.moneyDetails.get(index);
-    }
+	@Override
+	public MoneyDetail getItem(int index) {
+		return this.moneyDetails.get(index);
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-        OrderViewHolder viewHolder;
-        if (row == null) {
-            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = inflater.inflate(R.layout.cash_card_detail_item, parent, false);
-            viewHolder = new OrderViewHolder();
-            viewHolder.fbdNumber = (TextView) row.findViewById(R.id.fbdNumber);
-            viewHolder.amount = (TextView) row.findViewById(R.id.amount);
-            row.setTag(viewHolder);
-        } else {
-            viewHolder = (OrderViewHolder) row.getTag();
-        }
-        MoneyDetail moneyDetail = getItem(position);
-        viewHolder.fbdNumber.setText(getContext().getResources().getString(R.string.fbd_number, moneyDetail.getFBDNumber()));
-        viewHolder.amount.setText(getContext().getResources().getString(R.string.total_amount, moneyDetail.getAmount()));
-        return row;
-    }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		OrderViewHolder viewHolder;
+		if (row == null) {
+			LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			row = inflater.inflate(R.layout.cash_card_detail_item, parent, false);
+			viewHolder = new OrderViewHolder();
+			viewHolder.fbdNumber = (TextView) row.findViewById(R.id.fbdNumber);
+			viewHolder.amount = (TextView) row.findViewById(R.id.amount);
+			viewHolder.collectionAmount = (TextView) row.findViewById(R.id.collectionAmount);
+			viewHolder.serviceFee = (TextView) row.findViewById(R.id.serviceFee);
+			row.setTag(viewHolder);
+		} else {
+			viewHolder = (OrderViewHolder) row.getTag();
+		}
+		MoneyDetail moneyDetail = getItem(position);
+		viewHolder.fbdNumber.setText(getContext().getResources().getString(R.string.fbd_number, moneyDetail.getFBDNumber()));
+		viewHolder.amount.setText(getContext().getResources().getString(R.string.total_amount, moneyDetail.getAmount()));
+		viewHolder.collectionAmount.setText(getContext().getResources().getString(R.string.collectionAmount, moneyDetail.getMoneyCollectionAmount()));
+		viewHolder.serviceFee.setText(getContext().getResources().getString(R.string.serviceFee, moneyDetail.getNetTotal()));
+		return row;
+	}
 }
