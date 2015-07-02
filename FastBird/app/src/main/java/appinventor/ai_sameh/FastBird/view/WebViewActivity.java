c222dd4611ff80dd70ea782101956465fa108d3a
Suresh Kumar <suresh.kumar@rebtel.com>
2015-07-02 15:01:48 +0200

    update

--- FastBird/app/src/main/java/appinventor/ai_sameh/FastBird/view/WebViewActivity.java
+++ FastBird/app/src/main/java/appinventor/ai_sameh/FastBird/view/WebViewActivity.java
@@ -18,75 +18,75 @@ import appinventor.ai_sameh.FastBird.R;
 @SuppressLint("SetJavaScriptEnabled")
 public class WebViewActivity extends Activity {
 
-    private static final String TAG = WebViewActivity.class.getSimpleName();
-    private WebView paymentWebView;
-    private java.lang.String webUrl;
-    private ProgressBar progress;
-    private boolean isChat;
+	private static final String TAG = WebViewActivity.class.getSimpleName();
+	private WebView paymentWebView;
+	private java.lang.String webUrl;
+	private ProgressBar progress;
+	private boolean isChat;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_PROGRESS);
-        setContentView(R.layout.payment);
-        if (getIntent().getExtras() != null) {
-            isChat = true;
-            webUrl = getIntent().getExtras().getString("url");
-        }
-        if (TextUtils.isEmpty(webUrl)) {
-            webUrl = String.format("http://www.fastbird.org/m/BuyCredits.aspx?username=%s&password=%s", PreferenceUtil.getEmail(this), PreferenceUtil.getPassword(this));
-        }
-        initview();
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_PROGRESS);
+		setContentView(R.layout.payment);
+		if (getIntent().getExtras() != null) {
+			isChat = true;
+			webUrl = getIntent().getExtras().getString("url");
+		}
+		if (TextUtils.isEmpty(webUrl)) {
+			webUrl = String.format("http://www.fastbird.org/m/BuyCredits.aspx?username=%s&password=%s", PreferenceUtil.getEmail(this), PreferenceUtil.getPassword(this));
+		}
+		initview();
+	}
 
-    private void initview() {
-        findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        progress = (ProgressBar) findViewById(R.id.progressBar);
-        initWebView();
-        paymentWebView.setWebViewClient(new MyWebViewClient());
-        WebSettings webSettings = paymentWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setSupportZoom(true);
-        webSettings.setBuiltInZoomControls(true);
-        paymentWebView.loadUrl(webUrl);
-    }
+	private void initview() {
+		findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				finish();
+			}
+		});
+		progress = (ProgressBar) findViewById(R.id.progressBar);
+		initWebView();
+		paymentWebView.setWebViewClient(new MyWebViewClient());
+		WebSettings webSettings = paymentWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setSupportZoom(true);
+		webSettings.setBuiltInZoomControls(true);
+		paymentWebView.loadUrl(webUrl);
+	}
 
-    private void initWebView() {
-        paymentWebView = (WebView) findViewById(R.id.paymentWebView);
-        paymentWebView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
-    }
+	private void initWebView() {
+		paymentWebView = (WebView) findViewById(R.id.paymentWebView);
+		paymentWebView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
+	}
 
-    private class MyWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (isChat) {
-                if (url.equals("http://24bh.com/livehelp/offline.php?LANGUAGE=en")) {
-                    setResult(1);
-                    finish();
-                    return false;
-                }
-            }
-            view.loadUrl(url);
-            return true;
-        }
+	private class MyWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (isChat) {
+				if (url.equals("http://24bh.com/livehelp/offline.php?LANGUAGE=en")) {
+					setResult(1);
+					finish();
+					return false;
+				}
+			}
+			view.loadUrl(url);
+			return true;
+		}
 
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            progress.setVisibility(View.GONE);
-            WebViewActivity.this.progress.setProgress(100);
-            super.onPageFinished(view, url);
-        }
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			progress.setVisibility(View.GONE);
+			WebViewActivity.this.progress.setProgress(100);
+			super.onPageFinished(view, url);
+		}
 
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            progress.setVisibility(View.VISIBLE);
-            WebViewActivity.this.progress.setProgress(0);
-            super.onPageStarted(view, url, favicon);
-        }
-    }
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			progress.setVisibility(View.VISIBLE);
+			WebViewActivity.this.progress.setProgress(0);
+			super.onPageStarted(view, url, favicon);
+		}
+	}
 }
